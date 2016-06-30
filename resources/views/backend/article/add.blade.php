a54ae1836fffb9b70f08ff818b64c2d18396714f
Birim Karaustaoglu <bk@doubl.de>
2016-06-30 22:46:06 +0200

    fix breadcrumb links

--- resources/views/backend/article/add.blade.php
+++ resources/views/backend/article/add.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('title') {{ trans('backend/article.add') }} @endsection
 <?php $breadcrumb  = [
-    ['href' => URL::to('backend/articles'), 'title' => trans('backend/article.articles')],
-    ['href' => URL::to('backend/article/add'), 'title' => trans('backend/article.add')]
+    ['href' => URL::to('backend/c/article'), 'title' => trans('backend/article.articles')],
+    ['href' => URL::to('backend/c/article/a/add'), 'title' => trans('backend/article.add')]
 ]; ?>
--- resources/views/backend/article/all.blade.php
+++ resources/views/backend/article/all.blade.php
@@ -1,3 +1,3 @@
 @extends('master')
 @section('title') {{ trans('backend/article.articles') }} @endsection
-<?php $breadcrumb  = [['href' => URL::to('backend/articles'), 'title' => trans('backend/article.articles')]]; ?>
+<?php $breadcrumb  = [['href' => URL::to('backend/article'), 'title' => trans('backend/article.articles')]]; ?>
